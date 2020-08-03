9567adf78a5a522ae0c33d22743f9f15f034eedb
Huong Dang <miley.dawn@codeimmersives.com>
2020-08-03 16:24:39 -0400

    HW

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-1
+const p = document.querySelector('p')
+p.style.color = 'blue'
+document.querySelector('h1').style.fontSize = '5em'
+
+p.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+document.querySelector('#item-13').style.opacity = '50%'
+document.querySelector('#item-3').innerHTML = 'I say, "Hi!"'
+
+const img = document.querySelector('img')
+img.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg'
+img.height = '300'
+
+const img2 = document.querySelector('.img-2')
+img2.src = 'https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697'
+img2.height = '300'
+
+document.querySelector('ul').lastElementChild.innerHTML = "Won't get fooled again."
+
